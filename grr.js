f72660e5e0a78fffd6c7d2010519cfbf9818786b
Duncan Marsh <dwolfm@gmail.com>
2015-08-11 11:56:54 -0700

    yee its looking alot cleaner

--- grr.js
+++ grr.js
@@ -8,24 +8,19 @@ var ok = function(param, callback){
 }
 
 var dashAll = function(param, callback){
-  console.log("hit dashAll");
-  console.log('param in dashall', param);
-
   var abspath = path.resolve(param);
-  console.log('abspath', abspath);
   fs.exists(abspath, function(exists){
     if (!exists) return callback('file does not exists');
     callback();
-    console.log('soup slug');
   });
 } 
 
 yepok(process.argv.splice(2), {_: ok, a: dashAll , all: dashAll}, function(err, minimist){
  
   if (err) {
-    console.log('return the err');
-    console.error(err)
+    console.log('ERROR:');
+    return console.error(err)
   }
-  console.log('return the minimist');
+  console.log('MINIMIST:');
   console.log(minimist);
 });
